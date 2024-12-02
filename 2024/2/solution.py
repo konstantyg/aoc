3da7e6503319edb9149fbbdc81ffa7d8718792f1
Grzegorz Konstantynowicz <grzegorz.konstantynowicz@paramount.com>
2024-12-02 09:27:57 +0100

    day 2 2024

---
+++ 2024/2/solution.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+#     --- Day 2: Red-Nosed Reports ---
+#   https://adventofcode.com/2024/day/2
+
+
+InputType = list[list[int]]
+
+
+def is_safe(level):
+    diffs = [a - b for a, b in zip(level, level[1:])]
+    if any(d == 0 or abs(d) > 3 for d in diffs):
+        return False
+    has_inc = any(d > 0 for d in diffs)
+    has_dec = any(d < 0 for d in diffs)
+    if has_inc and has_dec:
+        return False
+    return True
+
+
+def part1(data: InputType):
+    return len([1 for level in data if is_safe(level)])
+
+
+def part2(data: InputType):
+    safe = 0
+    for level in data:
+        if is_safe(level):
+            safe += 1
+            continue
+        for i in range(len(level)):
+            tolerated = level[:i] + level[i + 1 :]
+            if is_safe(tolerated):
+                safe += 1
+                break
+    return safe
+
+
+def parse(input_data: str) -> InputType:
+    levels = []
+    for line in input_data.splitlines():
+        levels.append(list(map(int, line.split())))
+    return levels
+
+
+def main(input_data: str):
+    data = parse(input_data)
+    print("P1: ", part1(data))
+    print("P2: ", part2(data))
+
+
+if __name__ == "__main__":
+    main(open("input").read().strip())
